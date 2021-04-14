5d455fe26c38bf3e5bd8de226f3c7ce7f817bfa7
rubendmatos1985 <rubendmatos1985@gmail.com>
2021-04-14 17:35:12 +0200

    Agregar archivos de proyecto.

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    class Receiver
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "BasicTest", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(model: channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body.ToArray());
+                Console.WriteLine($"Received message {message}");
+            };
+
+            channel.BasicConsume(queue: "BasicTest", autoAck: true, consumer: consumer);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ Producer/Sender.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            using var connection = factory.CreateConnection();
+            
+            using var channel = connection.CreateModel();
+            
+            channel.QueueDeclare(queue: "BasicTest", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            
+            string message = "Getting started with .Net Core RabbitMQ";
+            
+            var body = Encoding.UTF8.GetBytes(message);
+            
+            channel.BasicPublish(exchange: "", routingKey: "BasicTest", body: body);
+            
+            Console.WriteLine($"Sent message {message}");
+            
+            Console.ReadLine();
+        }
+    }
+}
